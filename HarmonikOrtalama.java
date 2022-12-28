e077f095f4464fd40e64c293dd12e17a2785672f
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-28 09:18:12 +0300

    Add files via upload

---
+++ HarmonikOrtalama.java
@@ -0,0 +1,15 @@
+package Diziler;
+
+public class HarmonikOrtalama {
+    public static void main(String[] args) {
+        double[] list = {1, 2, 3, 4, 5};
+        double sum = 0.0;
+        double harmonik;
+
+        for (int i = 0; i < list.length; i++) {
+            sum += 1 / list[i];
+        }
+        harmonik = list.length / sum;
+        System.out.println(harmonik);
+    }
+}
